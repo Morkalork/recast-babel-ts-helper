e999eed9e997f67af4f8df58f39b48740672a29d
morkalork <maffelu@gmail.com>
2024-08-24 16:16:01 +0200

    chore: added some documentation and examples

--- src/generate-ast-from-code/index.ts
+++ src/generate-ast-from-code/index.ts
@@ -4,6 +4,10 @@ import { parse } from "recast";
 /**
  * Generates an AST from a string of code
  *
+ * @example
+ * const code = `export const myFunction = (a: number, b: number): number => a + b;`;
+ * const ast = generateAstFromCode(code);
+ * 
  * @param code A string of code to generate an AST from
  * @returns An AST of the code
  */
--- src/parse-function-or-class-name/index.ts
+++ src/parse-function-or-class-name/index.ts
@@ -6,6 +6,17 @@ import { FunctionNameDefinition } from "./types";
 /**
  * This function takes in a string of code or an AST and returns the name of the function or class, and a boolean indicating if it is a class.
  *
+ * @example
+ * const code = `export const myFunction = (a: number, b: number): number => a + b;`;
+ * const ast = generateAstFromCode(code);
+ *
+ * const results = parseFunctionOrClass(ast);
+ *
+ * const node = results[0];
+ * const parameters = node.parameters;
+ *
+ * // [{"name":"a","type":"number","tsType":"TSNumberKeyword","isOptional":false},{"name":"b","type":"number","tsType":"TSNumberKeyword","isOptional":false}]
+ *
  * @param code Either a string representation of the code to parse, or an AST of the code.
  * @returns A FunctionNameDefinition object with the name of the function or class, and a boolean indicating if it is a class.
  * @throws Error if the code does not correspond to a standard AST, or if the code does not contain a function or class.
